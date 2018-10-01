295a64d5cc74182b60bebb2304f4cfe9741a3eba
Matheus Brandino <m.v.brandino@gmail.com>
2018-10-01 12:02:22 -0300

    mostra a lista com recycler view

---
+++ app/src/main/java/br/com/caelum/casadocodigo/fragment/ListaLivrosFragment.java
@@ -0,0 +1,50 @@
+package br.com.caelum.casadocodigo.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.caelum.casadocodigo.R;
+import br.com.caelum.casadocodigo.adapter.LivroAdapter;
+import br.com.caelum.casadocodigo.modelo.Livro;
+
+public class ListaLivrosFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_lista_livros, container, false);
+
+        RecyclerView lista = view.findViewById(R.id.lista_livros);
+
+        List<Livro> livros = new ArrayList<>();
+
+        for (int i = 1; i <= 20; i++) {
+            Livro livro = new Livro();
+            livro.setNome("" + i);
+            livros.add(livro);
+        }
+
+        lista.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        lista.setAdapter(new LivroAdapter(livros));
+
+
+        return view;
+
+
+    }
+}
